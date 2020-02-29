9b4197353609f73c46bcd606cb99b69036f469b0
antirandominette <55027779+antirandominette@users.noreply.github.com>
2020-02-29 14:57:03 +0100

    Added for loops exercices qnd updated index.html

---
+++ Exercices/JS-assignments/loops-problem-set.js
@@ -0,0 +1,25 @@
+console.log("Problem 1\n\n");
+
+for(let i = -10; i <= 19; i++) {
+    console.log(i);
+}
+
+console.log("Problem 2\n\n");
+
+for(let j = 10; j <= 40; j += 2) {
+    console.log(j);
+}
+
+console.log("Problem 3\n\n");
+
+for(let k = 301; k <= 333; k += 2) {
+    console.log(k);
+}
+
+console.log("Problem 4\n\n");
+
+for(let l = 5; l <= 50; l++) {
+    if(l % 5 === 0 && l % 3 === 0) {
+        console.log(l);
+    }
+}
