47ef1465f57bc59d5685bb0bcda0cee770c46b84
antirandominette <55027779+antirandominette@users.noreply.github.com>
2020-03-10 18:10:21 +0100

    Updated ex

---
+++ Exercices/JS-assignments/array-iterations.js
@@ -0,0 +1,17 @@
+// let colors = ["Red", "Blue", "Orange", "Green"];
+
+// function callItem(array) {
+//     for(var i = 0; i < array.length; i++) {
+//         alert(array[i]);
+//         console.log(array[i]);
+//     }
+// }
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let colors = ["red", "orange", "yellow", "green"];
+
+for(let i = 0; i < numbers.length; i++) {
+    if(numbers[i] % 3 === 0) {
+        console.log(numbers[i]);
+    }
+}
