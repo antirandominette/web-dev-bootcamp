d7be55fb98b9abe067f99f7ef1fa07c755c79ad1
antirandominette <55027779+antirandominette@users.noreply.github.com>
2020-03-13 11:38:23 +0100

    Updated todoList Code Along

--- CodeAlong/todo-list.js
+++ CodeAlong/todo-list.js
@@ -2,28 +2,40 @@ window.setTimeout(() => {
     let todos = [];
     let input = prompt("What would you like to do ?");
 
+    function createTodo() {
+        let newTodo = prompt("What's the name of this todo");
+        todos.push(newTodo);
+        console.log(`Added ${newTodo} entry!`);
+    }
+
+    function deleteTodo() {
+        let index = prompt("Enter index of todo to delete");
+        todos.splice(index, 1);
+        alert(`Todo ${index} deleted!`);
+        console.log(`Deleted ${index} entry!`);
+    }
+
+    function listTodo() {
+        console.log("**********");
+        todos.forEach(function(todo, i) {
+            console.log(i + ": " + todo);
+        });
+        console.log("**********\n\n");
+    }
+
     while(input !== "quit") {
-
         if(input === "list") {
-            console.log("**********");
-
-            todos.forEach(function(todo, i) {
-                console.log(i + ": " + todo);
-            });
-
-            console.log("**********\n\n");
+            listTodo();
         }
         else if(input === "new") {
-            let newTodo = prompt("What's the name of this todo");
-            todos.push(newTodo);
+            createTodo();
         }
         else if(input === "delete") {
-            
+            deleteTodo();
         }
 
         input = prompt("What would you like to do ?");
     }
 
     console.log("QUITTING THE APP");
-}, 500)
-
+}, 500);
