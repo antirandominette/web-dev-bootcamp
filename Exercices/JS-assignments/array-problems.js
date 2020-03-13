f07de3553df896ef5bb467b4eb1a6fd7bc158f8d
antirandominette <55027779+antirandominette@users.noreply.github.com>
2020-03-13 12:55:02 +0100

    Added array-prblems and updated index.html

---
+++ Exercices/JS-assignments/array-problems.js
@@ -0,0 +1,29 @@
+function printReverse(array) {
+    let i;
+
+    i = array.length - 1;
+    while(i >= 0) {
+        console.log(array[i]);
+        i--;
+    }
+}
+
+function isUniform(array) {
+    console.log([...new Set(array)].length === 1 ? true : false);
+}
+
+function sumArray(array) {
+    let sum;
+
+    sum = 0;
+    for(let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum;
+}
+
+function max(array) {
+    array.sort((a, b) => a - b);
+
+    console.log(array[array.length - 1]);
+}
